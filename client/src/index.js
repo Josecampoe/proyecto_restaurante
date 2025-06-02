00dc4f8107c42dd57e5cd2c0b6e023a60fb06fce
Josecampoe <Jose.campoe@campusucc.edu.co>
2025-06-01 23:32:11 -0500

    Simplify React components for debugging

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { CssBaseline } from '@mui/material';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <CssBaseline />
-    <App />
-  </>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 ); 
